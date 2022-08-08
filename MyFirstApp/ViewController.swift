25321450b30b4b98ba52626b251371158b6084c5
Pavel Shirokiy <pshirokiy@gmail.com>
2022-08-08 15:04:21 +0300

    ui fixing

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var finalColorView: UIView!
+    
+    @IBOutlet var bluePercent: UILabel!
+    @IBOutlet var greenPercent: UILabel!
+    @IBOutlet var redPercent: UILabel!
+    
+    @IBOutlet var redColorJar: UISlider!
+    @IBOutlet var blueColorJar: UISlider!
+    @IBOutlet var greenColorJar: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        finalColorView.layer.cornerRadius = 20
+        
     }
 
-
+    private func setupLables() {
+        //bluePercent.text = Double(round(100 * blueColorJar.value) / 100)
+        
+    }
+    
+    @IBAction func addSomeRed() {
+        redPercent.text = String(Double(round(100 * redColorJar.value) / 100))
+    }
+    
+    @IBAction func addSomeGreen() {
+        greenPercent.text = String(greenColorJar.value)
+    }
+    
+    @IBAction func addSomeBlue() {
+        bluePercent.text = String(blueColorJar.value)
+    }
 }
 
