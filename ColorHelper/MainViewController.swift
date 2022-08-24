310662af73c91e0caa89d0bb54a4e10cf5c451fc
Pavel Shirokiy <pshirokiy@gmail.com>
2022-08-24 20:20:15 +0300

    new UI for app

---
+++ ColorHelper/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  ColoHelper
+//
+//  Created by 19543442 on 23.08.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
+        
+    }
+
+}
---
+++ ColorHelper/SettingsViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 19543442 on 25.07.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    
+    @IBOutlet var finalColorView: UIView!
+    
+    @IBOutlet var bluePercent: UILabel!
+    @IBOutlet var greenPercent: UILabel!
+    @IBOutlet var redPercent: UILabel!
+    
+    @IBOutlet var redColorJar: UISlider!
+    @IBOutlet var blueColorJar: UISlider!
+    @IBOutlet var greenColorJar: UISlider!
+    
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+
+// MARK: - View LifeStyle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        finalColorView.layer.cornerRadius = 20
+        setupLables()
+        setupTextFields()
+    
+    }
+
+// MARK: - IBActions
+    
+    
+    @IBAction func colorSlidersMash(_ sender: UISlider) {
+        finalColorView.backgroundColor = UIColor(
+        red: CGFloat(redColorJar.value),
+        green: CGFloat(greenColorJar.value),
+        blue: CGFloat(blueColorJar.value),
+        alpha: 1)
+    
+    redPercent.text = String(format: "%.2f", redColorJar.value)
+    redTextField.text = String(format: "%.2f", redColorJar.value)
+    greenPercent.text = String(format: "%.2f", greenColorJar.value)
+    greenTextField.text = String(format: "%.2f", greenColorJar.value)
+    bluePercent.text = String(format: "%.2f", blueColorJar.value)
+    blueTextField.text = String(format: "%.2f", blueColorJar.value)
+        
+    }
+    
+    
+// MARK: - Private func
+    
+    private func setupLables() {
+        bluePercent.text = String(blueColorJar.value)
+        redPercent.text = String(redColorJar.value)
+        greenPercent.text = String(greenColorJar.value)
+    }
+    
+    private func setupTextFields() {
+        redTextField.text = redPercent.text
+        greenTextField.text = greenPercent.text
+        blueTextField.text = bluePercent.text
+    }
+}
+
